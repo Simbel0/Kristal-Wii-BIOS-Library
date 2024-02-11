18a8a8a1c956438247666bcf5ac7a467ea1c4657
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2024-02-11 17:00:35 -0500

    Modification to getVii()

--- lib.lua
+++ lib.lua
@@ -44,12 +44,30 @@ function Lib:getVii()
 		Kristal.Console:warn("[Wii BIOS] Kristal is not currently in Wii Mode.")
 	end
 	
-	if self.wii_data then
-		if self.wii_data["vii"] then
-			return self.wii_data["vii"]
+	local wii_data = love.filesystem.read("wii_settings.json")
+	local wii_data_dc = JSON.decode(wii_data)
+	
+	if wii_data_dc then
+		if wii_data_dc["vii"] then
+			return wii_data_dc["vii"]
 		end
 	end
-	return false
+	Kristal.Console:warn("[Wii BIOS] Player has not made a Vii.")
+	return {
+		head = 1,
+		body = 1,
+		legs_left = true,
+		skin_color = {195/255, 195/255, 195/255},
+		hair_color = {61/255, 18/255, 14/255},
+		shirt_color_1 = {127/255, 127/255, 127/255},
+		shirt_color_2 = {1, 1, 1},
+		name = "VESSEL",
+		food = 1,
+		blood = 1,
+		color = 1,
+		gift = 1,
+		feel = 1
+	}
 end
 
 return Lib
